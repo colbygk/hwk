f50f8a8fd417903a85c71ba7b7a65205cb9f42be
Colby Gutierrez-Kraybill <hgranger@unm.edu>
2014-09-12 14:27:05 -0600

    Updated changes to new DSL

--- lib/hwk.rb
+++ lib/hwk.rb
@@ -2,100 +2,39 @@
 #
 #
 
-require 'singleton'
 
-require 'problem'
+require 'hwk/hwkfiles'
+require 'hwk/parser'
 
-class Homework
-  include Singleton
-  attr_accessor :title, :due_date, :class_name, :class_time, :author_name
-  attr_accessor :code_inclusion_configuration, :class_professor
-  attr_reader :identifier
+module Hwk
+  EXTNAME = 'hwk'
+  TEMPLATE_DIR = "~/Dropbox/Maths/latex/hwk/tex/"
+  TEMPLATE_FILE = "homework_template.tex"
+  FQP_TEMPLATE_FILE = File.join(TEMPLATE_DIR,TEMPLATE_FILE)
+  NEW_HWK_FILE  = "template.hwk"
 
-  def initialize
-    @debug = false
-    @title = "No title Set"
-    @due_date = Date.new
-    @class_name = "No class_name Set"
-    @class_time = "No class_time Set"
-    @class_professor = "No class_professor Set"
-    @author_name = "No author_name Set"
-    @code_inclusion_configuration = "% no code"
+  class Homework
+    attr_reader :parser, :files
+    attr_accessor :parsed_tex
 
-    @identifier = { :prefix => 'HWK_', :suffix => '_HWK' }
-    @ident_substr = @identifier[:prefix].length..(-1*(@identifier[:suffix].length+1))
-    @ident_regex = /#{@identifier[:prefix]}\S+#{@identifier[:suffix]}/
-
-    @problems = []
-  end
-
-  def add_problem( problem )
-    @problems << problem
-  end
-
-  def resolve_call( c )
-    begin
-      v = eval( "Homework.instance."+c )
-      if ( v.is_a?(String) )
-        v = escape_string_for_latex( v )
-      elsif ( v.is_a?(Array) )
-        vs = ""
-        v.each do |e|
-          vs << e.to_s
-        end
-        v = vs
-      elsif v.is_a?(Date)
-        v = v.strftime("%a, %e %b %Y %H:%M")
-      end
-    rescue NoMethodError
-      v = "Unknown reference: " + c
-    rescue TypeError => te
-      abort( "TypeError at "+ te.backtrace[0].split(':')[0..1].join(':') + " call:" + c )
-    rescue NameError => ne
-      abort( "NameError at "+ ne.backtrace[0].split(':')[0..1].join(':') + " call:" + c )
-    end
-    v
-  end
-
-  def escape_string_for_latex( s )
-    if s.is_a?(String)
-      s.gsub(/([&$#_{}~^])/, '\\\\\1')
-    elsif s == nil
-      ""
-    else
-      s
-    end
-  end
-
-  def problems_loop
-    problem_block = []
-
-    @problems.each do |problem|
-      problem_block << problem
+    def initialize( directory )
+      @files = HwkFiles.new( directory )
+      @parser = Parser.new
+      @tex = nil
     end
 
-    problem_block
-  end
-
-  def parse_template( template )
-    tex_output = ""
-    File.open( File.expand_path( template ) ).each do |line|
-      if @debug then print "read: #{line}" end
-      subs = line.scan(@ident_regex)
-      if subs then
-        if @debug then puts " found: #{subs}" end
-        subs.each do |c|
-          call = c[@ident_substr].downcase
-          value = resolve_call( call )
-          line = line.gsub(/#{c}/, value )
-        end
-      end
-      if @debug then print line end
-      tex_output << line
+    def load
+      @parser.load_dsl files.dsl
+      @parsed_tex = @parser.parse_template( FQP_TEMPLATE_FILE )
     end
 
-    tex_output
+    def write_tex
+      output_file = File.open( @files.tex, "w" )
+      output_file.write( @parsed_tex )
+      output_file.close
+    end
+      
+
   end
 
 end
-
--- lib/hwk/question.rb
+++ lib/hwk/question.rb
@@ -1,33 +1,84 @@
 
 # yoinked from: http://raycodingdotnet.wordpress.com/2013/08/11/writing-domain-specific-langaugedsl-in-ruby-day-1/
 
-require 'generalizedproblem'
-require 'subproblem'
 
-class Problem < GeneralizedProblem
+module Hwk
 
-  def initialize &block
-    super &block
-    enumstyle( '\alph' )
-  end
+  class Question
+    attr_accessor :im_a_part
+    [:enumstyle, :text,:solution, :parts].each do |m|
+      self.class_eval( %Q\
+        def #{m}(param=nil,&block)
+           param.nil?  ? (return @#{m}) : (@#{m} = param)
+           (instance_eval &block) if block_given?
+        end
+                      \ )
 
-  def to_s
-    problem_str = '\begin{homeworkProblem}' << "\n"
-    problem_str << question.to_s << "\n\\par\n"
+      def initialize( q, &block )
+        @enumstyle = '\alph'
+        @im_a_part = nil
+        call_stack = caller(2)
+        if call_stack
+          @im_a_part = call_stack[0].scan(/`(\S+)'/)[0][0]
+        end
 
-    if ( solution.to_s.length > 0 )
-      problem_str << '\problemAnswer{' << solution.to_s << "}\n"
-    end
-
-    if ( subproblems && subproblems.length > 0 )
-      problem_str << '\begin{enumerate}[label=(' << enumstyle << '*)]' << "\n"
-      subproblems.each do |subp|
-        problem_str << subp.to_s << "\n"
+        @text = q
+        @parts = []
+        (block.arity < 1 ?  (instance_eval &block) : block.call(self)) if block_given?
       end
-      problem_str << '\end{enumerate}' << "\n"
-    end
 
-    problem_str << '\end{homeworkProblem}' << "\n" 
-    problem_str
+      def part( q, &block )
+        @parts << Question.new( q, &block )
+      end
+
+      def to_s
+        puts( "current enum:#{enumstyle}")
+        if @im_a_part == 'part'
+          to_s_part_question
+        else
+          to_s_base_question
+        end
+      end
+
+      def to_s_part_question
+        question_str = '\item '
+        question_str << text.to_s << "\n\\par\n"
+
+        if ( solution.to_s.length > 0 )
+          question_str << '\problemAnswer{' << '\setlength\parindent{1em} \hangindent=1em' << solution.to_s << "}\n"
+        end
+
+        if ( @parts && @parts.length > 0 )
+          question_str << '\begin{enumerate}[label=(' <<  enumstyle << '*)]'
+          @parts.each do |p|
+            question_str << p.to_s << "\n"
+          end
+          question_str << '\end{enumerate}' << "\n"
+        end
+
+       question_str
+      end
+
+      def to_s_base_question
+        question_str = '\begin{homeworkProblem}' << "\n"
+        question_str << text.to_s << "\n\\par\n"
+
+        if ( @solution.to_s.length > 0 )
+          question_str << '\problemAnswer{' << '\setlength\parindent{1em} \hangindent=1em' << solution.to_s << "}\n"
+        end
+
+        if ( @parts && @parts.length > 0 )
+          question_str << '\begin{enumerate}[label=(' << enumstyle << '*)]' << "\n"
+          @parts.each do |p|
+            question_str << p.to_s << "\n"
+          end
+          question_str << '\end{enumerate}' << "\n"
+        end
+
+        question_str << '\end{homeworkProblem}' << "\n" 
+        question_str
+      end
+    end
   end
+
 end
